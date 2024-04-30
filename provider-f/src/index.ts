b2e01e78e3c15575b1491b70dbeeddb8edf3418e
Thoushif Aazam Shaik <aazam555@gmail.com>
2024-04-30 13:42:54 -0500

    added two node projects - f and c

---
+++ provider-f/src/index.ts
@@ -0,0 +1,6 @@
+import { ApiClient as F_ApiClient, ApiError as F_ApiError } from "./api";
+
+import { sum as F_simpleSUm } from "./demo1/demo1";
+import { sum as F_sumwithargs } from "./demo2/demo2";
+
+export { F_ApiClient, F_ApiError, F_simpleSUm, F_sumwithargs };
